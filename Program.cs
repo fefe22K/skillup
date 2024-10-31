54bca795f70bb82b2a75372d33630c8f48ce5dfc
Heitor Martins <heitor.candido3@portalsesisp.org.br>
2024-10-31 10:30:29 -0300

    email enviando sozinho

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using System.Reflection;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using skill_up.Context;
 using skill_up.Models;
 
@@ -17,7 +19,31 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFramework
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Cadastros de cursos e funcionários ",
+        Description = "Uma Api para cadastro de cursos e funcionários wn ASP.net",
+        TermsOfService = new Uri("https://example.com/license"),
+        Contact = new OpenApiContact
+        {
+            Name = "Suporte",
+            Url = new Uri("https://example.com/contact")
+        },
+         License = new OpenApiLicense
+        {
+            Name = "Licença de uso",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    var xmlNome = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,xmlNome));
+});
+ 
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("azure")));
 
 
@@ -44,6 +70,13 @@ builder.Services.AddAuthorization(options =>
 });
 
 builder.Services.AddCors(options => { options.AddPolicy(MyAllowSpecificOrigins, policy => { policy.WithOrigins("*") .AllowAnyHeader() .AllowAnyMethod(); }); }); 
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
